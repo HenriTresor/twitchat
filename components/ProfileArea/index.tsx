5f28ede7a746af52bf0c39bd36736af9a4f12323
henritresor <shimwamanahenritresor@gmail.com>
2023-07-30 13:57:01 +0200

    edited profile component

--- components/ProfileArea/index.tsx
+++ components/ProfileArea/index.tsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 type Props = {}
 
-function ProfileArea({}: Props) {
-  return (
-    <div className='aside border-l-4'>ProfileArea</div>
-  )
+function ProfileArea({ }: Props) {
+    return (
+        <div className='aside border-l-4 flex flex-col items-center  overflow-auto gap-5'>
+            <div className='w-[100px] h-[100px] bg-gray-400 mt-5 rounded-full'>
+
+            </div>
+            <h1 className='font-extrabold tracking-wider text-center text-[1.3rem]'>Full name</h1>
+
+            <ul className='list-none w-full'>
+                <li className='action-btn'>Sociala profile</li>
+                <li className='action-btn border-red-500 text-red-500'>Block</li>
+            </ul>
+        </div>
+    )
 }
 
 export default ProfileArea
