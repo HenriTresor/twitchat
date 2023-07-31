40c7015532254bc6df1417e8253be4011f393390
henritresor <shimwamanahenritresor@gmail.com>
2023-07-31 13:25:59 +0200

    removed chat header component from the side bar

--- components/ContactsArea/index.tsx
+++ components/ContactsArea/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ChatHeader from '../ChattingArea/ChatHeader'
-import SideBar from './SideBar'
 import Contact from './Contact'
 
 type Props = {}
@@ -8,9 +7,10 @@ type Props = {}
 function ContactsArea({ }: Props) {
   return (
     <div className='aside border-r-4  flex-row flex w-auto md:w-[30%] xlg:hidden'>
-      <SideBar />
       <div className='w-full overflow-auto bar hidden md:block'>
-        <ChatHeader />
+        <div className='w-full h-auto'>
+          Twitchat
+        </div>
         <div className=''>
           <Contact />
           <Contact />
