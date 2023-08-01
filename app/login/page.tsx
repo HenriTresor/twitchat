4e1f68934e55cbde51d476ff45366e0a1d42c442
henritresor <shimwamanahenritresor@gmail.com>
2023-08-01 10:49:41 +0200

    finished landing page initialised login page

---
+++ app/login/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Image from 'next/image'
+import React, {useEffect} from 'react'
+
+type Props = {}
+
+function Login({ }: Props){
+
+
+    const handleSubmit = (): void => {
+
+    }
+    return (
+        <div className='w-full min-h-screen flex items-center justify-center'>
+            <div className='p-4 shadow-lg w-[50%] h-auto flex border-2'>
+                <div className='w-[50%] md:block hidden'>
+                    <h1>Continue With Sociala</h1>
+                    <Image
+                        width={'100'}
+                        src={'/login-avatar.png'}
+                        height={'100'}
+                        alt='avatar'
+                        className='w-full h-full object-contain'
+                    />
+                </div>
+                <div className='p-2 w-[50%]'>
+                    <div className='w-full h-auto p-2 flex flex-col gap-2'>
+                        <label htmlFor="email">Email address: </label>
+                        <input
+                            className='border w-full p-2 outline-none focus:border-4'
+                            type="email" placeholder='enter your email' id='email' name='email' />
+                    </div>
+                    <div className='w-full h-auto p-2 flex flex-col gap-2'>
+                        <label htmlFor="password">Password: </label>
+                        <input
+                            className='border w-full p-2 outline-none focus:border-4'
+                            type="password" placeholder='enter your password' id='password' name='password' />
+                    </div>
+                    <div className='p-2 mt-4'>
+                        <button className='btn p-2 border border-blue-300 rounded-md'>Continue...</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import { FaFacebookMessenger } from 'react-icons/fa'
+import { FaFacebookMessenger, FaGooglePlay, FaAppStore } from 'react-icons/fa'
 
 type Props = {}
 
@@ -14,24 +14,58 @@ function LandingPage({}: Props) {
                   </h1>
                   <h1 className='font-verdana text-[1.6rem] tracking-wider '>Twitchat</h1>
               </div>
-              <ul className='flex items-center gap-2'>
+              <ul className='md:flex items-center gap-2 hidden'>
                   <li className='list-item'>developers</li>
                   <li className='list-item'>about us</li>
                   <li className='list-item'>Contact us</li>
               </ul>
           </div>
 
-          <div>
-              <h1>Sociala Chat</h1>
-              <p>Chat easily with your friends on sociala here ...</p>
-
-              <div>
+          <div className='w-full  flex p-2 items-center min-h-screen justify-center'> 
+              <div className='w-[100%] p-5 md:w-[50%] ' >
+                  <div className='flex items-center gap-4'>
+                      <h1 className='text-blue-300 text-[2rem] md:text-[3rem]'>
+                          <FaFacebookMessenger />
+                      </h1>
+                      <h1 className='font-mono text-[2rem] md:text-[3rem] tracking-wider text-blue-700'>Sociala Chat</h1>
+                 </div>
+                  <p className='font-sans mt-5  tracking-wider md:text-[1.5rem] text-[0.9rem]'>Chat easily with your friends on sociala here ...</p>
+                  <p className='font-sans mt-5  tracking-wider md:text-[1.5rem] text-[0.9rem]'>Have all your chats in one place!</p>
+                  <button className='btn  mt-10 p-4 pr-8 rounded-xl border-2 border-blue-500 text-verdana text-[1.2rem] tracking-widest'>
+                      Continue with Sociala
+                  </button>
+              </div>
+              <div className='w-[50%] md:block hidden'>
                   <Image
-                      src={'/login-avatar.png'}
                       width={'100'}
+                      src={'/login-avatar.png'}
                       height={'100'}
                       alt='avatar'
+                      className='w-full h-full object-contain'
                   />
+              </div>
+          </div>
+
+          <div className='w-full h-auto p-4 flex items-center flex-col md:flex-row gap-4 justify-around '>
+              <div className='flex gap-2 flex-col items-start' >
+                  <h4>Download Twitchat on:</h4>
+                  <div className='flex gap-2'>
+                      <button className='btn flex items-center gap-2 p-2 rounded-md border-2 border-blue-200 text-verdana text-[1rem] tracking-wider'>
+                          <FaGooglePlay />
+                          Play Store
+                      </button>
+                      <button className='btn flex items-center gap-2 p-2 rounded-md border-2 border-blue-200 text-verdana text-[1rem] tracking-wider'>
+                          <FaAppStore />
+                          App Store
+                      </button>
+                 </div>
+              </div>
+
+              <div>
+                  <p>&copy; Twitchat Copyright, 2023</p>
+              </div>
+              <div>
+                  <p>Sociala Inc.</p>
               </div>
           </div>
     </div>
