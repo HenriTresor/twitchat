ebe5a6b8a1d42ec676f911854d5f32692e9027fb
henritresor <shimwamanahenritresor@gmail.com>
2023-08-01 11:13:28 +0200

    added fetch function to login from the server

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from 'next/image'
 import React, { useReducer } from 'react'
+import { useRouter } from 'next/router'
 
 type Props = {}
 
@@ -11,15 +12,34 @@ interface inputValues {
 
 function Login({ }: Props) {
 
+    const { push } = useRouter()
     const [inputValues, setInputValues] = React.useState<inputValues>({
         email: '',
         password: ''
     })
-    const handleSubmit = (): void => {
+
+    const handleChange = (e: any): void => {
+        setInputValues((prev: inputValues) => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+    const handleSubmit = async (): Promise<any> => {
         try {
 
-        } catch (error) {
+            const res = await fetch('http://localhost:8080/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(inputValues)
+            })
+            const data = await res.json()
+            if (data.status) {
 
+                document.cookie = `access_token=${data.access_token}`
+                push('/t')
+                return
+            }
+        } catch (error: any) {
+            alert(error.message)
         }
     }
     return (
@@ -39,18 +59,21 @@ function Login({ }: Props) {
                     <div className='w-full h-auto p-2 flex flex-col gap-2'>
                         <label htmlFor="email">Email address: </label>
                         <input
+                            onChange={(e) => handleChange(e)}
                             className='border w-full p-2 outline-none focus:border-4'
                             type="email" placeholder='enter your email' id='email' name='email' />
                     </div>
                     <div className='w-full h-auto p-2 flex flex-col gap-2'>
                         <label htmlFor="password">Password: </label>
                         <input
+                            onChange={(e) => handleChange(e)}
                             className='border w-full p-2 outline-none focus:border-4'
                             type="password" placeholder='enter your password' id='password' name='password' />
                     </div>
                     <div className='p-2 mt-4'>
                         <button className='btn p-2 border border-blue-300 rounded-md' onClick={handleSubmit}>Continue...</button>
                     </div>
+
                 </div>
             </div>
         </div>
