f65c3ddd57d2dfe00e0d5aebf2f71aee17de727b
henritresor <shimwamanahenritresor@gmail.com>
2023-08-02 13:39:33 +0200

    changed message typing ui

--- components/ChattingArea/index.tsx
+++ components/ChattingArea/index.tsx
@@ -15,7 +15,7 @@ function ChattingArea({ }: Props) {
             <ChatBody />
             {
                 currentChat && (
-                    <div className='w-full flex border-t-2 p-2 justify-start'>
+                    <div className='w-full flex border-t-2 p-2 justify-start items-start'>
                         <textarea name="message" id="message" rows={1.2} className='flex-grow border-2 border-slate-500 mr-2 resize-y p-2 outline-none' placeholder={`try, "hello ${currentChat?.sender?.names || currentChat?.users[0]?._id !== currentUser?._id && currentChat?.users[0]?.names}"`}></textarea>
                         <button className='btn bg-blue-500 text-white rounded-sm p-2'>
                             <MdSend />
